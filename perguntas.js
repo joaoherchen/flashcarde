d7b211e062b57e50db1e6b621b421ab5dcac49f5
joaoherchen <joao.herchen@escola.pr.gov.br>
2025-06-18 14:09:55 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,47 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Anime Bleach',
+    'Qual o nome da bankai do Yamamoto?',
+    'Zanka no Tachi'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Anime Bleach',
+    'Qual o nome do vice capitão a devisão 11?',
+    'Yachiru Kusajishi'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Anime Naruto',
+    'Qual o nome das bijus de uma cauda até a de dez caudas ?',
+    'Shukaku, Matatabi, Isobu, Son Goku, Kokuo, Saiken, Chomei, Gyuki, Kurama, Juubi'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Anime Naruto',
+    'Quem criou a bijus?',
+    'Hagoromo Ōtsutsuki'
+)
+
+criaCartao(
+    'Anime One Piece',
+    'Qual o nome verdadeiro da fruta do luffy?',
+    'Hito Hito no Mi, Modelo: Nika'
+)
+
+criaCartao(
+    'Anime One Piece ',
+    'Quais as frutas do Barba Negra?',
+    ' Yami Yami no Mi e Gura Gura no Mi'
+)
+
+criaCartao(
+    'Anime Dragon Ball',
+    'Qual a forma mais forte do Goku?',
+    ' Instinto Superior Completo'
+)
+
+criaCartao(
+    'Anime Dragon Ball',
+    'Qual a forma mais forte do Vegeta?',
+    'Ultra Ego ou Ego Superior'
 )
